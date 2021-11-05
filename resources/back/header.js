94f898392dbd09f3b71f88808e42a06be5ff2a0d
GustavoAlves <93629115+gustavoalvees@users.noreply.github.com>
2021-11-05 12:47:18 -0300

    back

--- resources/back/header.js
+++ resources/back/header.js
@@ -5,13 +5,8 @@ var tM = 2;
 
 
 
-document.querySelector('#header').addEventListener('click', function(event) {
-  /*// find the closest parent of the event target that
-  // matches the selector
-  var closest = event.target.closest(selector);
-  if (closest && base.contains(closest)) {
-    // handle class event
-  }*/ 
-  //document.getElementsByClassName("dash-1")
-
-});
+document.body.addEventListener('click', function (event) {
+    if(event.target.className.contains("dash")){
+    	console.log(event.target.className)
+    }
+}, false);
