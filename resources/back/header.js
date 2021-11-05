ac81c58125c1cbb3da8917f8f78c436b9e209b5a
GustavoAlves <93629115+gustavoalvees@users.noreply.github.com>
2021-11-05 12:56:56 -0300

    back

--- resources/back/header.js
+++ resources/back/header.js
@@ -6,7 +6,5 @@ var tM = 2;
 
 
 document.body.addEventListener('click', function (event) {
-    if(event.target.className.contains("dash")){
-    	console.log(event.target.className)
-    }
+    console.log(menu)
 }, false);
