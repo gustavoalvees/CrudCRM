b41a3a2d754e70a406d32c0aa8515f6450c24600
GustavoAlves <93629115+gustavoalvees@users.noreply.github.com>
2021-11-05 12:38:02 -0300

    back

--- resources/back/header.js
+++ resources/back/header.js
@@ -1,1 +1,17 @@
 let menu = 0;
+var tM = 2;
+
+
+
+
+
+document.querySelector('#header').addEventListener('click', function(event) {
+  /*// find the closest parent of the event target that
+  // matches the selector
+  var closest = event.target.closest(selector);
+  if (closest && base.contains(closest)) {
+    // handle class event
+  }*/ 
+  //document.getElementsByClassName("dash-1")
+
+});
