1c29a667e44a0ce2ef6131c7213229cb5bdfb1b2
gustavoalvees <93629115+gustavoalvees@users.noreply.github.com>
2022-01-10 21:18:21 -0300

    fixed

--- resources/back/menusHandler.js
+++ resources/back/menusHandler.js
@@ -1,23 +1,24 @@
 let menu = 0; // global
 var tMenu = 0; // local temp 
 
-var headerTabs  = 3;
-var clientsTabs = 1;
-
 document.body.addEventListener('click', function (event) {
     if(menu != tMenu){ 
         console.log(menu)
         if(menu >= 0 && menu < 4){// tab main
             document.getElementsByClassName("handlerClick")[tMenu].classList.remove("active");
             document.getElementsByClassName("handlerClick")[menu].classList.add("active") ;
-            for (var i=0; i <= headerTabs; i++) {
+            for (var i=0; i <= 3; i++) {
                 document.getElementsByClassName('header-'+i)[0].style.display = "none";
             }
             document.getElementsByClassName('header-'+menu)[0].style.display = "block";
         }
         if(menu > 3){// tab client
             document.getElementsByClassName("handlerClick")[tMenu].classList.remove("active");
-            document.getElementsByClassName("handlerClick")[menu].classList.add("active") ;
+            document.getElementsByClassName("handlerClick")[menu].classList.add("active");
+
+            document.getElementsByClassName('client-5')[0].style.display = "none";
+            document.getElementsByClassName('client-4')[0].style.display = "none";
+            document.getElementsByClassName('client-'+menu)[0].style.display = "block";
         }   
         tMenu = menu;
     }
