4a73358a9a21908dcda13cc84452c13ce2ce0bc3
gustavoalvees <93629115+gustavoalvees@users.noreply.github.com>
2022-01-12 14:16:58 -0300

    ok

--- resources/back/contain.js
+++ resources/back/contain.js
@@ -1,6 +1,14 @@
-function event(){
-	var text = "";
-	var searchv = " \
+let data = "";
+
+//var $ = jQuery = require('jquery');
+//require('datatables.net')();
+
+function xs(d){
+    var text;
+    if(d==1)
+      text = ""
+    else {text = document.getElementsByClassName("search-clientes")[0].value;}
+    var searchv = " \
       SELECT * \
       FROM clientes \
       WHERE nome  LIKE '%"+text+"%' OR\
@@ -13,44 +21,32 @@ function event(){
             db = new SQL.Database(fileBuffer(file));
             var d = db.exec(searchv)
             var table = document.getElementsByClassName("table")[0];
-            table.innerHTML = '\
-            			<thead class="thead-dark">\
-						   	<tr>\
-						    	<th scope="col">#</th>\
-						    	<th scope="col">Nome</th>\
-						    	<th scope="col">Email</th>\
-						    	<th scope="col">Telefone</th>\
-						    	<th scope="col">CPF</th>\
-						    	<th scope="col">Ação</th>\
-							</tr>\
-						</thead>';
+            table.innerHTML = '<tr>\
+                  <th scope="col">#</th>\
+                  <th scope="col">Nome</th>\
+                  <th scope="col">Email</th>\
+                  <th scope="col">Telefone</th>\
+                  <th scope="col">CPF</th>\
+                  <th scope="col">Ação</th>\
+              </tr>';
             try {
-            	for (var i = 0; i < d[0].values.length; i++) {
-            		var row = table.insertRow(1);
-            		row.scope = "row";
-            		row.insertCell(0).innerHTML = d[0].values[i][0];
-            		row.insertCell(1).innerHTML = d[0].values[i][1];
-            		row.insertCell(2).innerHTML = d[0].values[i][2];
-            		row.insertCell(3).innerHTML = d[0].values[i][3];
-            		row.insertCell(4).innerHTML = d[0].values[i][4];
-            		row.insertCell(5).innerHTML = "<button></button>";
-            	}
+              for (var i = 0; i < d[0].values.length; i++) {
+                var row = table.insertRow(0);
+                row.insertCell(0).innerHTML = d[0].values[i][0];
+                row.insertCell(1).innerHTML = d[0].values[i][1];
+                row.insertCell(2).innerHTML = d[0].values[i][2];
+                row.insertCell(3).innerHTML = d[0].values[i][3];
+                row.insertCell(4).innerHTML = d[0].values[i][4];
+                row.insertCell(5).innerHTML = "<a>button</a>";
+
+              }
             }catch (e) {
-            	console.log(e)
+              console.log(e)
             }
         });
+
 }
-event()
+xs(1)
 document.getElementsByClassName("search-clientes")[0].addEventListener('keypress', function (event) {
-	event()
+  xs(0);
 });
-
-/*
-var table = document.getElementById("myTable");
-  var row = table.insertRow(0);
-  var cell1 = row.insertCell(0);
-  var cell2 = row.insertCell(1);
-  cell1.innerHTML = "NEW CELL1";
-  cell2.innerHTML = "NEW CELL2";
-
-*/
